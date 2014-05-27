00d53149a7bfa352323aac23fe593530755671ec
Marc Ignacio <marcrendlignacio@gmail.com>
2014-05-27 22:22:52 +0800

    Notes should fail when there is no date

--- spec/do_by_spec.rb
+++ spec/do_by_spec.rb
@@ -14,17 +14,8 @@ describe DoBy::Note do
       ENV["ENABLE_DO_BY"] = nil
     end
 
-    it "is happy with a note without a date" do
-      expect { DoBy::Note.new('foo') }.to_not raise_error
-    end
-
     it "is happy with a date in the future" do
       expect { DoBy::Note.new('foo', '2014-06-01 19:00') }.to_not raise_error
-    end
-
-    it "is not happy with a date in the past" do
-      expect { DoBy::Note.new('foo', '2014-06-01 18:00') }.
-        to raise_error(DoBy::LateTask, "foo is overdue (2014-06-01 18:00)")
     end
 
     it "supports making a TODO note" do
@@ -40,6 +31,16 @@ describe DoBy::Note do
     it "supports making a OPTIMIZE note" do
       expect(DoBy::Note).to receive(:new).with('fix this', '2012-01-01')
       OPTIMIZE 'fix this', '2012-01-01'
+    end
+
+    it "is not happy with a date in the past" do
+      expect { DoBy::Note.new('foo', '2014-06-01 18:00') }.
+        to raise_error(DoBy::LateTask, "foo is overdue (2014-06-01 18:00)")
+    end
+
+    it "is not happy with a note without a date" do
+      expect { DoBy::Note.new('foo') }.
+        to raise_error(DoBy::NoDueDateTask, "missing due date")
     end
   end
 
